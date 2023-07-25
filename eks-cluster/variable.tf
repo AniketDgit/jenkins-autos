db31e0c1238077b5f0ebfb4651f89feec9f3926e
AniketDgit <137816964+AniketDgit@users.noreply.github.com>
2023-07-25 23:54:28 +0530

    Create variable.tf

---
+++ eks-cluster/variable.tf
@@ -0,0 +1,9 @@
+variable "private_subnet_cidr_blocks" {
+    type = list(string)
+}
+variable "public_subnet_cidr_blocks" {
+    type = list(string)
+}
+variable "vpc_cidr_block" {
+    type = string  
+}
